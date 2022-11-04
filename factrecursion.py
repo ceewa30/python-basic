8bb820d8324f4a90edac594f4f64e8417efe544b
SIVAKUMAR SANTHALINGM <ceewa30@gmail.com>
2022-11-04 10:50:00 -0400

    factorial update

--- factrecursion.py
+++ factrecursion.py
@@ -1,5 +1,5 @@
 def factorial(n):
-    assert n >=0 and imt(n) == n, 'The number must be a positive integer only!'
+    assert n >=0 and int(n) == n, 'The number must be a positive integer only!'
     if n in [0,1]:
         return 1
     else:
