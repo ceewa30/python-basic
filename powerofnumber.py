2ecf20565d053608090902ff4581329e7d5e7ed4
SIVAKUMAR SANTHALINGM <ceewa30@gmail.com>
2022-11-08 13:59:19 -0500

    Interview Question

--- powerofnumber.py
+++ powerofnumber.py
@@ -1,8 +1,3 @@
-from tkinter import N
-
-from symbol import flow_stmt
-
-
 # how to calculate power of a number using Recursion
 
 # x ^ n = x * x * x * ... (n times) .. ^ n 
@@ -16,4 +11,25 @@ from symbol import flow_stmt
 # x^3 * x^4 = x^ 3+4
 
 # x^n = x * x^n-1
+
+# step 2: Base case - the stopping criterion
+
+# n = 0
+
+# step 3: Unintentional case - the constraint
+
+# power(-1,2) ??
+# power(3.2,2) ??
+# power(2,1.2) ??
+# power(2,-1) ??
+
+def power(base, exp):
+    assert int(exp) == exp, 'The exponent must be integer number only'  # power(2,1.2) ??
+    if exp == 0:
+        return 1
+    elif exp < 0:
+        return 1/base * power(base, exp+1)  # power(2,-1) ??
+    return base * power(base, exp-1)
+
+print(power(2,2))
 
