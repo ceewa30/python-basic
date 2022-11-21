f447a32b8e91c3392f2aef6d403844f7ac9d705f
SIVAKUMAR SANTHALINGM <ceewa30@gmail.com>
2022-11-21 18:45:25 -0500

    one dimenstion array practice

---
+++ arrayPractive.py
@@ -0,0 +1,102 @@
+from array import *
+
+# 1. Create an array and traverse.
+
+my_array = array('i',[1,2,3,4,5])
+
+for i in my_array:
+    print(i)
+
+
+# 2. Access individual elements through indexes.
+
+print("step 2")
+print(my_array[0])
+
+
+# 3. Append any value to the array using append() method.
+
+print("Step 3")
+my_array.append(6)
+print(my_array)
+
+# 4. Insert value in an array using insert() method.
+
+print("Step 4")
+my_array.insert(2,8)
+print(my_array)
+
+
+# 5. Extend python array using extend() method.
+my_arr = array('i',[23,45,3])
+
+print("Step 5")
+my_array.extend(my_arr)
+print(my_array)
+
+# 6. Add items from list into array using fromlist() method.
+
+print("Step 6")
+tempList = [23,54,23]
+my_array.fromlist(tempList)
+print(my_array)
+
+# 7. Remove any array element using remove() method.
+
+print("Step 7")
+my_array.remove(23)
+print(my_array)
+
+# 8. Remove last array element using pop() method.
+
+print("Step 8")
+my_array.pop()
+print(my_array)
+
+
+# 9. Fetch an element through its index using index() method.
+
+print("Step 9")
+print(my_array.index(23))
+
+# 10. Reverse a python array using reverse() method.
+
+print("Step 10")
+my_array.reverse()
+print(my_array)
+
+# 11. Get array buffer information through buffer_info() method.
+
+print("Step 11")
+print(my_array.buffer_info())
+
+
+# 12. Check for number if occurrences of an element using count() method.
+
+print("Step 12")
+print(my_array.count(23))
+
+
+# 13. Convert array to string using tostring() replaced tobytes method.
+
+print("Step 13")
+strTemp = my_array.tobytes()
+print(strTemp)
+ints = array('i')
+ints.frombytes(strTemp)
+print(ints)
+
+
+# 14. Convert array to a python list with same elements using tolist() method.
+
+print("Step 14")
+#print(my_array.tolist())
+
+# 15. Append a string to char array using fromstring() replaced frombytes method.
+
+print("Step 15")
+
+# 16. Slice elements from an array.
+
+print("Step 16")
+print(my_array[1:4])
